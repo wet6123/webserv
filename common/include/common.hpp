034a761164e7eb5c965d690025455c34ee983aae
donggyun112 <102022855+donggyun112@users.noreply.github.com>
2024-07-10 15:24:27 +0900

    [COMMON] config | Add COMMON value
    * [DOCS] config | add COMMON value
    
    * [COMMON] feat | Add server status macro and refactor data types using namespaces for improved organization and type safety
    
    * [COMMON] feat | Add server status macro and refactor data types using namespaces for improved organization and type safety
    
    * [COMMON] chore | move header guard string_utils.cpp -> string_utils.hpp
    
    * [DOCS] chore | Remove unnecessary code in test.cpp

---
+++ common/include/common.hpp
@@ -0,0 +1,31 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <list>
+
+#include "types/data_type.hpp"
+#include "utils/string_utils.hpp"
+
+
+
+
+namespace WEB_SERVER {
+	namespace STRING_UTILS = ::STRING_UTILS;
+	namespace Type = ::Type;
+	namespace State = ::State;
+	namespace Method = ::Method;
+	namespace Status = ::Status;
+}
+
+
+#endif
