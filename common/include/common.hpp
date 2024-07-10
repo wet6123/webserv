bcf32241ebf69809abe59b2d95b8f258c55e50e1
donggyun112 <ssddgg99@daum.net>
2024-07-10 19:46:07 +0900

    [COMMON] feat | Implement ErrorHandling class for logging and error handling

--- common/include/common.hpp
+++ common/include/common.hpp
@@ -12,12 +12,13 @@
 #include <queue>
 #include <stack>
 #include <list>
+#include <ctime>
+#include <iomanip>
+#include <ctime>
+#include <cstdlib>
 
 #include "types/data_type.hpp"
 #include "utils/string_utils.hpp"
-
-
-
 
 namespace WEB_SERVER {
 	namespace STRING_UTILS = ::STRING_UTILS;
@@ -27,5 +28,57 @@ namespace WEB_SERVER {
 	namespace Status = ::Status;
 }
 
+namespace WEB_SERVER {
+    class ErrorHandling {
+    public:
+        enum LogLevel {
+            DEBUG,
+            INFO,
+            WARNING,
+            ERROR,
+            FATAL
+        };
+
+    protected:
+        LogLevel currentLogLevel;
+		std::ofstream logFile;
+		
+
+        virtual const char* getLogLevelString(LogLevel level) const;
+
+        virtual std::string getCurrentTimestamp() const;
+
+        virtual void writeLog(const std::string& logMessage);
+
+    public:
+        ErrorHandling();
+		ErrorHandling(const std::string &logFileName);
+        virtual ~ErrorHandling() {}
+
+        void setLogLevel(LogLevel level);
+
+        virtual void log(LogLevel level, const std::string &message, const char *file, int line);
+
+        void debug(const std::string &message, const char *file, int line) {
+            log(DEBUG, message, file, line);
+        }
+
+        void info(const std::string &message, const char *file, int line) {
+            log(INFO, message, file, line);
+        }
+
+        void warning(const std::string &message, const char *file, int line) {
+            log(WARNING, message, file, line);
+        }
+
+        void error(const std::string &message, const char *file, int line) {
+            log(ERROR, message, file, line);
+        }
+
+        void fatal(const std::string &message, const char *file, int line) {
+            log(FATAL, message, file, line);
+        }
+    };
+}
 
 #endif
--- common/src/common.cpp
+++ common/src/common.cpp
@@ -1,1 +1,117 @@
 #include "../include/common.hpp"
+
+
+WEB_SERVER::ErrorHandling::ErrorHandling() : currentLogLevel(DEBUG) {}
+
+
+WEB_SERVER::ErrorHandling::ErrorHandling(const std::string &logFileName) : currentLogLevel(DEBUG)
+{
+	logFile.open(logFileName.c_str(), std::ios::out | std::ios::app);
+}
+/**
+* @brief 로그 파일에 로그를 기록합니다.
+* @param logMessage 로그 메시지
+* @return void
+*/
+void WEB_SERVER::ErrorHandling::writeLog(const std::string &logMessage)
+{
+	std::cout << logMessage << std::endl;
+	if (logFile.is_open()) {
+		logFile << logMessage.c_str() << std::endl;
+	}
+}
+/**
+* @brief 로그 레벨을 문자열로 변환합니다.
+* @param level 로그 레벨
+* @return 로그 레벨 문자열
+*/
+const char * WEB_SERVER::ErrorHandling::getLogLevelString(LogLevel level) const
+{
+	switch (level)
+	{
+	case DEBUG: return "DEBUG";
+	case INFO: return "INFO";
+	case WARNING: return "WARNING";
+	case ERROR: return "ERROR";
+	case FATAL: return "FATAL";
+	default: return "UNKNOWN";
+	}
+}
+
+/**
+* @brief 현재 타임스탬프를 반환합니다.
+* @return 현재 타임스탬프
+*/
+std::string WEB_SERVER::ErrorHandling::getCurrentTimestamp() const
+{
+	time_t now = time(0);
+	char buffer[80];
+	strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", localtime(&now));
+	return std::string(buffer);
+}
+
+/**
+* @brief 현재 로그 레벨을 설정합니다.
+* @param level 로그 레벨
+*/
+void WEB_SERVER::ErrorHandling::setLogLevel(LogLevel level)
+{
+	currentLogLevel = level;
+}
+/**
+ * @brief 로그를 출력합니다.
+ * @param level 로그 레벨
+ * @param message 로그 메시지
+ * @param file 파일명
+ * @param line 라인 번호
+ * @return void
+*/
+void WEB_SERVER::ErrorHandling::log(LogLevel level, const std::string &message, const char *file, int line)
+{
+	if (level < currentLogLevel) return;
+
+	std::ostringstream ss;
+	ss << "[" << getCurrentTimestamp() << "] "
+		<< "[" << getLogLevelString(level) << "] "
+		<< file << ":" << line << " - " << message << std::endl;
+
+	writeLog(ss.str());
+
+	if (level == FATAL) {
+		std::exit(1);
+	}
+}
+
+/**
+ * @brief 디버그 로그를 출력합니다.
+ * @param message 로그 메시지
+ * @param file 파일명
+ * @param line 라인 번호
+ * @return void
+*/
+
+void WEB_SERVER::ErrorHandling::debug(const std::string &message, const char *file, int line)
+{
+	log(DEBUG, message, file, line);
+}
+
+void WEB_SERVER::ErrorHandling::info(const std::string &message, const char *file, int line)
+{
+	log(INFO, message, file, line);
+}
+
+void WEB_SERVER::ErrorHandling::warning(const std::string &message, const char *file, int line)
+{
+	log(WARNING, message, file, line);
+}
+
+void WEB_SERVER::ErrorHandling::error(const std::string &message, const char *file, int line)
+{
+	log(ERROR, message, file, line);
+}
+
+void WEB_SERVER::ErrorHandling::fatal(const std::string &message, const char *file, int line)
+{
+	log(FATAL, message, file, line);
+}
+
