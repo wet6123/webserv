c1873478582633f8084c3c89149b419ed3899eb4
Yechan kim <dpcks5959@naver.com>
2024-07-16 18:11:27 +0900

    [COMMON] style | Organized Common functions (#20)
    * first commit
    
    * [Parsing] feat | created ConfigManager and StringReader
    
    * [Parsing]
    Feat : Add server block parsing functionality
    
    Added funtionality to parse server blocks from config files.
    But this is not quite finished yet, Need some renovations.
    Renovations are include Location parsing func, code refactory, and additional tests.
    
    * [Parsing]
    feat | Add Location block parsing func
    
    Although location pars func, not completed.
    need to addtional test.
    
    * [Parsing]
    fix | fixed ConfigManager, related classes and funcs
    
    ConfigManager, StringReader and util functions were fixed.
    Still not quite finished yet.
    
    * [Parsing]
    fix | fixed parse functions
    
    Fixed parse functions that include server and location.
    Additionally, added funcs for writing comment to config file.
    
    * [Parsing]
    feat | added parsing func
    
    Added config file parsing functions.
    Servers and locations are accessible via ConfigManager.
    If you want to get a location, use server's getLocation() method.
    Server block in the conf file must contain listen(port), root_path, client_body_size, default_index_path, host(address) and default_error_page_path.
    Location block in the conf file must contain uri_path.
    Should you write '#' in conf file, those after '#' will automatically be commented.
    
    * [Parsing] feat | Implementing StringReader Class
    
    * [Parsing] feat | Implemented StringReader Class
    
    * [Parsing] feat | Implemented StringReader Class
    
    * [COMMON] style | changed StringReader's location from Header and Sources to Common/utils
    
    ---------
    
    Co-authored-by: YCK1204 <https://github.com/YCK1204>

---
+++ common/String.hpp
@@ -0,0 +1,24 @@
+#ifndef STRING_HPP
+# define STRING_HPP
+
+#include <vector>
+#include <cstdarg>
+
+#include "String/Reader.hpp"
+
+#define TRIM_SPACE " \n\v\t"
+#define GET_V_NAME(name) String::Split(#name, "::", false).back()
+
+namespace String 
+{
+    int ToInt(std::string str);
+    bool StartWith(std::string str, std::string find, bool trim = false);
+    std::string Trim(std::string str);
+    std::string Replace(std::string target, std::string oldStr, std::string newStr);
+    std::vector<std::string> Split(const std::string &str, const std::string &delim, bool);
+    std::string Format(std::string target, ...);
+    std::string Itos(int n);
+    class Reader;
+}
+
+#endif
--- common/utils/utils.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_HPP
-#define UTILS_HPP
-
-#include "split.hpp"
-
-#define GET_V_NAME(name) WEB_SERVER::UTILS::Split(#name, "::").back()
-
-#endif
